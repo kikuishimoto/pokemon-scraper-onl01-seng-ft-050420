df5c315657fd9bbd00d2b4bebd47c68d48a835d6
kiku ishimoto <kikuish@gmail.com>
2020-06-04 23:52:28 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -17,15 +17,12 @@ class Pokemon
   end
   def self.find(id, db)
     sql = <<-SQL
-      SELECT * FROM pokemon
-      WHERE id = ?
+        SELECT * FROM pokemon
+        WHERE id = ?
     SQL
     pokemon = db.execute(sql, id)
     new_pokemon = self.new(id: pokemon[0][0], name: pokemon[0][1] , type: pokemon[0][2], db: db)
-        # new_pokemon.id = pokemon[0][0]
-        # new_pokemon.name = pokemon[0][1]
-        # new_pokemon.type = pokemon[0][2]
-        # new_pokemon.db = db
+  
     new_pokemon
   end
 end
