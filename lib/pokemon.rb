326d78f48df49d0b8df6b81bb06b7701988e1bc7
kiku ishimoto <kikuish@gmail.com>
2020-06-04 23:20:59 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,21 @@
+
 class Pokemon
+attr_accessor :name, :type,:db
+attr_reader :id
+
+  def initialize (id: nil, name:, type:, db:)
+    @id = id
+    @name = name
+    @type = type
+    @db = db
+  end
+
+  def self.save(name, type, db)
+    sql = <<-SQL
+      INSERT INTO pokemon(name, type) VALUES (?, ?);
+      SQL
+
+      db.execute(sql, self.name, self.type)
+
+    end
 end
